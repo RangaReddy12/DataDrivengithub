6078c3932b851132e9d2999df8f2461c649d87d0
Qedge <Qedge@DESKTOP-1RLTEAV.Dlink>
2025-02-10 20:18:22 +0530

    New Class Created

---
+++ OrangeHRM_DDT/src/test/java/commmonFunctions/Sample.java
@@ -0,0 +1,22 @@
+package commmonFunctions;
+
+public class Sample {
+public void add()
+{
+	int a=876,b=9876,c;
+	c=a+b;
+	System.out.println(c);
+}
+public void division()
+{
+	int a=6656,b=7,c;
+	c=a+b;
+	System.out.println(c);
+}
+public void sub()
+{
+	int a=53464,b=7676,c;
+	c=a+b;
+	System.out.println(c);
+}
+}
